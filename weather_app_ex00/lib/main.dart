81ed05c44e8690521ea176ca41cba7996bf231c8
Seongjko <rhtjdwns2345@gmail.com>
2025-01-16 14:36:08 +0900

    feat: weather_app_ex00 completed

--- weather_app_ex00/lib/main.dart
+++ weather_app_ex00/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'WeatherUI.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const WeatherUI();
   }
 }
+
